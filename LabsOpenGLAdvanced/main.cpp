244cecae07a94a30c53c1cf48d927c7d40666abc
thereisnousername1 <126057736+thereisnousername1@users.noreply.github.com>
2025-02-25 22:22:22 +0000

    Lab 0 done

---
+++ LabsOpenGLAdvanced/main.cpp
@@ -0,0 +1,20 @@
+
+// order = 0, first item in the include
+#include <glad/glad.h>
+
+// order > 0
+#include <GLFW/glfw3.h>
+
+// order > 1
+
+#include <glm/glm.hpp>
+#include <iostream>
+
+// this lab shows the correct settings of an OpenGL project
+// with the library located at C:\Users\Public\OpenGL
+int main()
+{
+	// when everything is linked without bugs, the command prompt should output Hello World!
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
